55061b9984db40efb3f68d83cb7e1fbdd6fd3a61
Pooja Nayak <poojanayak.1994@gmail.com>
2017-10-14 19:05:17 +0530

    first commit

---
+++ springPractice/src/main/java/com/bridgelabz/springPractice/MyBean.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.springPractice;
+
+public class MyBean {
+private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ springPractice/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Pooja</h2>
+</body>
+</html>
